1cd733484d084cf9b25815dd5f462326d666e126
Jose Bernal <jbernal.web.dev@gmail.com>
2021-10-26 10:02:50 -0700

    Enable both test and prod configurations for the cart.

---
+++ js/OCDLA.js
@@ -0,0 +1,67 @@
+(function(window,undefined){
+
+	window.OCDLA = {};
+	
+	let config = {};
+	
+	let testDomains = ["ocdpartial-ocdla.cs217.force.com","membertest.ocdla.org"];
+	
+	
+	let production = {
+		storeCookieId: 'apex__00D6300000097I4EAI-fonteva-shopping-cart',
+		// 		storeCookieId: 'apex__00Dj0000001okn9EAA-fonteva-shopping-cart', Production
+		defaultCatalogId: 'a0lj0000000B7FTAA0', // A 'click' event is generated on this element (li > a.defaultCatalogId) so that this catalog is rendered by default; in the future store the user's active catalog in a cookie value so this can be persisted.
+		publicationCatalog: 'a0lj0000000B7FTAA0',
+		
+		domain: 'members.ocdla.org',
+		
+		businessDomain: 'www.ocdla.org',
+		
+		useForms: true
+	};
+	
+	
+	let test = {
+		storeCookieId: 'apex__00D6300000097I4EAI-fonteva-shopping-cart',
+		// 		storeCookieId: 'apex__00Dj0000001okn9EAA-fonteva-shopping-cart', Production
+		defaultCatalogId: 'a0lj0000000B7FTAA0', // A 'click' event is generated on this element (li > a.defaultCatalogId) so that this catalog is rendered by default; in the future store the user's active catalog in a cookie value so this can be persisted.
+		publicationCatalog: 'a0lj0000000B7FTAA0',
+		
+		domain: 'membertest.ocdla.org',
+		
+		businessDomain: 'test.ocdla.org',
+		
+		useForms: true
+	};
+
+	
+	// Set some alternate settings if on Test.
+	if(testDomains.includes(window.location.hostname)) {
+		config = test;
+	} else if(prodDomains.includes(window.location.hostname)) {
+		config = production;
+	} else {
+		console.log('WARNING: setting for hostname, '+window.location.hostname+', not found.');
+	}
+	
+	OCDLA.domain = config.domain;
+	
+	OCDLA.addLibrary = function(ns,lib){
+		this[ns] = lib;
+	};
+	
+	OCDLA._get = function(name){
+		return config[name]||null;
+	};
+	
+	OCDLA._set = function(name,val){
+		config[name] = val;
+	};
+	
+	OCDLA.pageName = function(){
+		return window.location.pathname;
+	};
+
+
+
+})(window,undefined);
